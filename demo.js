a9c03c125303afed9d89ec57bdcf947575a1b901
AlejoMejia96 <alejandroquinteromejia@gmail.com>
2023-06-16 19:00:28 -0500

    04 finished

--- demo.js
+++ demo.js
@@ -1,43 +1,34 @@
 //variables
 
-var numero = 123;
-
-var nombre = "Alejandro es el instructor";
-
+// var numero = 123;
+// var nombre = "Alejandro es el instructor";
 
 
 //Operadores
 
-var resultado = 2 + 7;
-
-
+// var resultado = 2 + 7;
 
 //Operadores
-var comparacion = 3 >= 4
-
-
+// var comparacion = 3 >= 4
 
 //logicos
 
-var resultadoLogico = (100 > 200)&&( 3 >= 3);
-
-
-
+// var resultadoLogico = (100 > 200)&&( 3 >= 3);
 
 //métodos
 
-var nombre = 'Alejo';
+// var nombre = 'Alejo';
 
-var numero = 9;
+// var numero = 9;
 
 // returnnumero.length) //cuenta los caracteres del stri
 
 //funciones
 
-function sumar (a, b) {
-  var suma = a + b;
-  return suma;
-}
+// function sumar (a, b) {
+//   var suma = a + b;
+//   return suma;
+// }
 
 // returnsumar(3, 4)
 // returnsumar(8, 9)
@@ -45,7 +36,7 @@ function sumar (a, b) {
 
 //condicionales
 
-var edad = 15;
+// var edad = 15;
 
 // if(edad > 18) {
 //   return'soy mayor de edad'
@@ -53,14 +44,86 @@ var edad = 15;
 //   return'soy menor de edad'
 // }
 
-function dominioDelPais(dominio){
-  if(dominio==="co"){
-    return 'el pais es colombia'
-  } else if (dominio==="br"){
-    return 'el pais es brasil'
-  } else {
-    return 'el pais es argentina'
-  }
-}
+// function dominioDelPais(dominio){
+//   if(dominio==="co"){
+//     return 'el pais es colombia'
+//   } else if (dominio==="br"){
+//     return 'el pais es brasil'
+//   } else {
+//     return 'el pais es argentina'
+//   }
+// }
 
-console.log(dominioDelPais('uy'));
+// console.log(dominioDelPais('uy'));
+
+//for
+
+// for (var i = 0; i < "hola".length; i++) {
+//   console.log("hola"[i]);
+// }
+
+// i = 0 --> 'h' 
+//i = 1 --> 'o'
+//...
+// i = 4 -->  termina el for
+
+// for (var i = 0; i >= 0; i++){
+//   console.log(i);
+// }
+
+// function encontrarVocalA(string){
+//   for(var i = 0; i < string.length; i++){
+//     if(string[i] === 'a'){
+//       return "Encontramos la vocal a"
+//     }
+//   }
+//   return "El string no tiene ninguna letra a"
+// }
+
+// console.log(encontrarVocalA('hola'))
+
+//while
+
+// var contador = 1; 
+//   while(contador <= 6) {
+//     contador++;
+//     return "contador es: ", contador;
+//   }
+
+//do while
+
+// var count = 6; 
+//   do {
+// 	 console.log("count es: " + count);
+//     	count++;
+//  }
+//   while(count < 6)
+
+//switch
+
+// function diaSemana(dia) {
+//   switch (dia) {
+//     case "lunes":
+//       console.log("Hoy es lunes");
+//       break;
+//     case "martes":
+//       console.log("Hoy es martes");
+//       break;
+//     case "miercoles":
+//       console.log("Hoy es miércoles");
+//       break;
+//     case "jueves":
+//       console.log("Hoy es jueves");
+//       break;
+//     case "viernes":
+//       console.log("Hoy es viernes");
+//       break;
+//     default:
+//       console.log("El día no es válido");
+//       break;
+//   }
+// }
+
+// console.log(diaSemana('Lunes'));
+
+console.log('hola');
